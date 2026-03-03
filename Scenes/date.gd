f1ef370fdf8e36876129260ad4f367c451c5948d
jchapps <josephchapple06@gmail.com>
2026-03-03 14:51:22 +0000

    feat : added file reading and storing for questions

--- Scenes/date.gd
+++ Scenes/date.gd
@@ -2,8 +2,36 @@ extends Node2D
 
 var connection = 0
 
+var sharktopus_questions1 = []
+var sharktopus_questions2 = []
+var sharktopus_questions3 = []
+
+var pteracuda_questions1 = []
+var pteracuda_questions2 = []
+var pteracuda_questions3 = []
+
+var whalewolf_questions1 = []
+var whalewolf_questions2 = []
+var whalewolf_questions3 = []
+
+var bearanha_questions1 = []
+var bearanha_questions2 = []
+var bearanha_questions3 = []
+
+var velocipastor_questions1 = []
+var velocipastor_questions2 = []
+var velocipastor_questions3 = []
+
+var kininja_questions1 = []
+var kininja_questions2 = []
+var kininja_questions3 = []
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	read_questions()
+	date_loop()
+	
 	pass # Replace with function body.
 
 
@@ -11,15 +39,79 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func read_questions() -> void:
+	var file = FileAccess.open("res://TextFiles/questions.txt", FileAccess.READ)
+	var section = 1
+	
+	# loops through entire file
+	while not file.eof_reached():
+		var line = file.get_line()
+		
+		# if line is blank
+		if line == "":
+			section = section + 1
+		else:
+			
+			# split line into array
+			var lineArray = line.split("; ")
+			
+			#sharktopus sections
+			if section == 1:
+				sharktopus_questions1.append(lineArray)
+			elif section == 2:
+				sharktopus_questions2.append(lineArray)
+			elif section == 3:
+				sharktopus_questions3.append(lineArray)
+				
+			#pteracuda sections
+			elif section == 4:
+				pteracuda_questions1.append(lineArray)
+			elif section == 5:
+				pteracuda_questions2.append(lineArray)
+			elif section == 6:
+				pteracuda_questions3.append(lineArray)
+			
+			#whalewolf sections
+			elif section == 7:
+				whalewolf_questions1.append(lineArray)
+			elif section == 8:
+				whalewolf_questions2.append(lineArray)
+			elif section == 9:
+				whalewolf_questions3.append(lineArray)
+				
+			#bearanha sections
+			elif section == 10:
+				bearanha_questions1.append(lineArray)
+			elif section == 11:
+				bearanha_questions2.append(lineArray)
+			elif section == 12:
+				bearanha_questions3.append(lineArray)
+				
+			#velocipastor sections
+			elif section == 13:
+				velocipastor_questions1.append(lineArray)
+			elif section == 14:
+				velocipastor_questions2.append(lineArray)
+			elif section == 15:
+				velocipastor_questions3.append(lineArray)
+				
+			#kininja sections
+			elif section == 4:
+				kininja_questions1.append(lineArray)
+			elif section == 5:
+				kininja_questions2.append(lineArray)
+			elif section == 6:
+				kininja_questions3.append(lineArray)
+			
 # the main gameplay loop
-func _date_loop() -> void:
+func date_loop() -> void:
 	
 	while connection < 100:
 		# hide all elements
-		$DateQA.visble = false
+		$DateQA.visible = false
 		$DateCards.visible = false
 		# display question elements
-		$DateQA.visble = true
+		$DateQA.visible = true
 		$DateQA/Questions/QuestionsBox/AnswersBox.visible = true
 		$DateQA/Questions/QuestionsBox/ContinueButton.visible = false
 	
@@ -37,7 +129,7 @@ func _date_loop() -> void:
 		# await continue to be pressed
 	
 		# hide question elements
-		$DateQA.visble = false
+		$DateQA.visible = false
 	
 		# display card elements
 		# create intention
@@ -48,8 +140,8 @@ func _date_loop() -> void:
 		# await end turn to be pressed
 		# act on intention
 		intention_act()
-	
 		# loop
+		connection = 100
 	pass
 	
 func question_ask() -> void:
